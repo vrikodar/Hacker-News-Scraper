e48fd188e22f2019a45f57338bcec2132763c3f7
SxNade <73271976+SxNade@users.noreply.github.com>
2020-11-01 05:21:46 -0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+os.system("pip3 install beautifulsoup4")
+os.system("pip3 install requests")
+os.system("pip3 install termcolor")
+os.system("cls")
+
+print('[!]Finished Installing all Dependencies..')
+sys.exit()
+
